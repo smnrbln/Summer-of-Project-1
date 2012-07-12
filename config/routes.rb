85cb94a4a7b94bc18b87ffeb6ddc55200299a76b
ftastemur <fatih.tastemur@bil.omu.edu.tr>
2012-07-12 21:28:42 +0300

    file_uploader

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SummerOfProject::Application.routes.draw do
   resources :sessions
   resources :projects
+  resources :upload
 
   get "login" => "sessions#new", :as => "login"
   get "logout" => "sessions#destroy", :as => "logout"
+  get 'upload/index'
 
   root :to => 'sessions#new'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120706115558) do
+ActiveRecord::Schema.define(:version => 20120710103749) do
+
+  create_table "data_files", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "projects", :force => true do |t|
     t.string   "title"
